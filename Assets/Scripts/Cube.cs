d22d2809d537d7d2bb64a15ff93c13bd6f44c183
VLADISLAV130 <a676202@gmail.com>
2024-05-16 16:38:44 +0500

    ExplosionsCubes2

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,47 +1,73 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
-{    
+{
     [SerializeField] private Cube _cube;
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] float _explosionForce;
-    [SerializeField] float _explosionRadius;    
 
+    private float _explosionForce = 10f;
+    private float _explosionRadius = 5f;
     private int _minCountCubes = 2;
     private int _maxCountCubes = 6;
-    private float _maxChanceOfDivision = 100f;    
-    private float currentChanceOfDivision = 100f;
+    private float _maxChanceOfDivision = 100f;
+    private float _currentChanceOfDivision = 100f;
 
     private void OnMouseDown()
     {
-        float _chanceOfDivision = Random.Range(0, _maxChanceOfDivision);
+        float chanceOfDivision = Random.Range(0, _maxChanceOfDivision);
 
-        if (_chanceOfDivision <= _cube.currentChanceOfDivision)
+        if (chanceOfDivision <= _cube._currentChanceOfDivision)
         {
             Init();
         }
-           
+        else
+        {
+            Explode(transform.position);
+        }
+
         Destroy(gameObject);
     }
 
     public void Init()
     {
+        float coefficient = 2f;
         int countCubes = Random.Range(_minCountCubes, _maxCountCubes);
 
         for (int i = 0; i < countCubes; i++)
         {
-            Cube cube = Instantiate(_cube);            
+            Cube cube = Instantiate(_cube);
 
-            cube.transform.localScale /= 2;
+            cube.transform.localScale /= coefficient;
             cube.GetComponent<Renderer>().material.color = Random.ColorHSV();
-            cube.currentChanceOfDivision /= 2f;
-
-            Explode(transform.position);
+            cube._currentChanceOfDivision /= coefficient;
+            cube._explosionForce *= coefficient;
+            cube._explosionRadius *= coefficient;
         }
     }
 
     private void Explode(Vector3 position)
     {
-        _rigidbody.AddExplosionForce(_explosionForce, position, _explosionRadius);
+        foreach (Rigidbody explodableObjects in GetExplodableObjects())
+        {
+            explodableObjects.AddExplosionForce(_explosionForce, position, _explosionRadius , 0f, ForceMode.Impulse);            
+        }        
+    }
+
+    private List<Rigidbody> GetExplodableObjects()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        List<Rigidbody> cubs = new();
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.attachedRigidbody != null)
+            {
+                cubs.Add(hit.attachedRigidbody);
+            }
+        }
+
+        return cubs;
     }
 }
